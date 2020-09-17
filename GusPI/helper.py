139a021303a3f8ab637c8a6a24a0c568f5970904
ygeszvain <34775549+ygeszvain@users.noreply.github.com>
2020-09-17 16:49:59 -0500

    0.0.56

--- GusPI/helper.py
+++ GusPI/helper.py
@@ -11,12 +11,12 @@ def _fit_predict_model(dataframe, interval_width=0.99, changepoint_range=0.8):
                 changepoint_range=changepoint_range)
     m = m.fit(dataframe)
 
-    forecast = m.predict(dataframe)
-    forecast['fact'] = dataframe['y'].reset_index(drop=True)
-    return forecast
+    pred = m.predict(dataframe)
+    pred['fact'] = dataframe['y'].reset_index(drop=True)
+    return pred
 
-def _detect_anomalies(forecast):
-    forecasted = forecast[['ds', 'trend', 'yhat', 'yhat_lower', 'yhat_upper', 'fact']].copy()
+def _detect_anomalies(pred):
+    forecasted = pred[['ds', 'trend', 'yhat', 'yhat_lower', 'yhat_upper', 'fact']].copy()
 
     forecasted['anomaly'] = 0
     forecasted.loc[forecasted['fact'] > forecasted['yhat_upper'], 'anomaly'] = 1
