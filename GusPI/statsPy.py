cc26dac366bd79724c5aad64e7c1326f38101904
ygeszvain <34775549+ygeszvain@users.noreply.github.com>
2020-09-25 23:14:33 -0500

    0.0.58

--- GusPI/statsPy.py
+++ GusPI/statsPy.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import helper
+from fbprophet import Prophet
 
 
 def init_df(df, colname, target, target_value):
@@ -113,6 +114,32 @@ def plot_benfordlaw(result, title='', figsize=(15, 8)):
 
     return fig, ax
 
+def _fit_predict_model(dataframe, interval_width=0.99, changepoint_range=0.8):
+    m = Prophet(daily_seasonality=False, yearly_seasonality=False, weekly_seasonality=False,
+                seasonality_mode='multiplicative',
+                interval_width=interval_width,
+                changepoint_range=changepoint_range)
+    m = m.fit(dataframe)
+
+    pred = m.predict(dataframe)
+    pred['mark'] = dataframe['y'].reset_index(drop=True)
+    return pred
+
+def _detect_anomalies(pred):
+    predicted = pred[['ds', 'trend', 'yhat', 'yhat_lower', 'yhat_upper', 'mark']].copy()
+
+    predicted['anomaly'] = 0
+    predicted.loc[predicted['mark'] > predicted['yhat_upper'], 'anomaly'] = 1
+    predicted.loc[predicted['mark'] < predicted['yhat_lower'], 'anomaly'] = -1
+
+    predicted['importance'] = 0
+    predicted.loc[predicted['anomaly'] == 1, 'importance'] = \
+        (predicted['mark'] - predicted['yhat_upper']) / forecast['mark']
+    predicted.loc[predicted['anomaly'] == -1, 'importance'] = \
+        (predicted['yhat_lower'] - predicted['mark']) / forecast['mark']
+
+    return predicted
+
 
 def annomalies_detection(df, colname, target):
     df = df.loc[df[colname] == target]
@@ -122,8 +149,8 @@ def annomalies_detection(df, colname, target):
     df = df.reset_index()
     df = df.dropna()
     df.columns = ['ds', 'y']
-    pred = helper.fit_predict_model(df)
-    pred = helper.detect_anomalies(pred)
+    pred = _fit_predict_model(df)
+    pred = _detect_anomalies(pred)
     pred_ano = pred.loc[pred['anomaly'] == 1]
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=pred['ds'], y=pred['fact'], name=target_col, mode='lines'))
